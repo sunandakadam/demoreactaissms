2605d3b30fef7481e1cc55c7124a8e0d17c62fb3
Sunanda <sunandak2007@gmail.com>
2022-03-16 10:19:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Parent from './Parent';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Hello World!!!</h2>
+     <Parent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Parent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Parent extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <p>This is parent component</p>
+            </div>
+        );
+    }
+}
+ 
+export default Parent;
